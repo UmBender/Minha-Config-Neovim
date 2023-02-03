ce1b3ce213eb2375a49ea4d0c438789283802d02
Gustavo Bender <gustavo.benderr@gmail.com>
2023-02-03 14:57:47 -0300

    Modificacao para realsar o escopo atual na identacao

--- after/plugin/indent.lua
+++ after/plugin/indent.lua
@@ -11,6 +11,8 @@ vim.opt.listchars:append "space: "
 vim.opt.listchars:append "eol: "
 
 require("indent_blankline").setup {
+	show_current_context = true,
+	-- show_current_context_start = true,
     space_char_blankline = " ",
     char_highlight_list = {
         "IndentBlanklineIndent1",
