24cfb84a43a857c1cde7d23dcf8898bcdec868c9
Gustavo Bender <gustavo.benderr@gmail.com>
2023-02-07 01:15:22 -0300

    Adicao do scrolloff de 8, para que a tela comece a mover com no minimo 8 linhas de diferenca

--- lua/bender/config.lua
+++ lua/bender/config.lua
@@ -2,6 +2,7 @@ vim.opt.relativenumber = true
 vim.opt.termguicolors  = true
 vim.opt.shiftwidth     = 4
 vim.opt.tabstop        = 4
+vim.opt.scrolloff      = 8
 
 vim.o.termguicolors    = true
 vim.o.cursorline       = true
