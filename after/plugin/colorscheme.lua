285386ecc16a0241071b2a35e4aa998d8531f81d
Gustavo Bender <gustavo.benderr@gmail.com>
2023-05-29 21:10:11 -0300

    configuração que eu nem me lembro mais

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -22,7 +22,7 @@ require('onedark').setup {
 -- require('onedark').load()
 --
 require("catppuccin").setup({
-    flavour = "macchiato", -- latte, frappe, macchiato, mocha
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
     -- background = { -- :h background
     --    light = "latte",
     --     dark = "mocha",
---
+++ after/plugin/floaterm.lua
@@ -0,0 +1,9 @@
+compile_type = {
+	["c"]= "gcc % -o %< && ./%<",
+	["cpp"]= "g++ % -o %< && ./%<"
+}
+vim.keymap.set('n', '<C-y>',":FloatermNew --autoclose=0 python3 % <CR>")
+-- vim.keymap.set('n', '<C-p>',":FloatermNew --autoclose=0 gcc % -o %< && ./%<<CR>")
+vim.keymap.set('n', '<C-p>',":FloatermNew --autoclose=0 g++ % -o %< && ./%<<CR>")
+vim.keymap.set('n', '<C-r>',":FloatermNew --autoclose=0 cargo run -q --color always <CR>")
+
