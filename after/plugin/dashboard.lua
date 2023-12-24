b882c8a606cff627f29e688383194ca11ecdce4b
Gustavo Bender <gustavo.benderr@gmail.com>
2023-12-24 13:24:14 -0300

    Better configuration of the dashboard

--- after/plugin/dashboard.lua
+++ after/plugin/dashboard.lua
@@ -1,8 +1,14 @@
 event = 'VimEnter'
+function reconfig()
+	vim.cmd("cd  ~/.config/nvim")
+	vim.cmd("edit ~/.config/nvim/lua/bender/config.lua")
+end
 require('dashboard').setup {
-    theme = 'hyper',
+    theme = 'doom',
     config = {
 		header = {
+			[[                                                  ]],
+			[[                                                  ]],
 			[[██████╗ ███████╗███╗   ██╗██████╗ ███████╗██████╗ ]],
 			[[██╔══██╗██╔════╝████╗  ██║██╔══██╗██╔════╝██╔══██╗]],
 			[[██████╔╝█████╗  ██╔██╗ ██║██║  ██║█████╗  ██████╔╝]],
@@ -15,26 +21,70 @@ require('dashboard').setup {
 			[[              ╚██╗ ██╔╝██║██║╚██╔╝██║             ]],
 			[[               ╚████╔╝ ██║██║ ╚═╝ ██║             ]],
 			[[                ╚═══╝  ╚═╝╚═╝     ╚═╝             ]],
+			[[                                                  ]],
+			[[                                                  ]],
 		},
-		center = 
-		{
-			{
-				icon = ' ',
-				icon_hl = 'Title',
-				desc = 'Find File',
-				key = '',
-				desc_hl = 'String',
-				keymap = 'Shift f',
-			},
-		},
-
-		week_header = {
-			enable = false,
-		},
-		footer = {
-		},
-
+		center = {
+      {
+        icon = '🗎 ',
+        icon_hl = 'Title',
+        desc = 'Find File                               ',
+        desc_hl = 'String',
+        key = 'f',
+        key_hl = 'Number',
+        key_format = ' %s', -- remove default surrounding `[]`
+        action = 'Telescope find_files'
+      },
+      {
+        icon = '🗋 ',
+        icon_hl = 'Title',
+        desc = 'New File',
+        desc_hl = 'String',
+        key = 'n',
+        key_hl = 'Number',
+        key_format = ' %s', -- remove default surrounding `[]`
+        action = 'tabnew'
+      },
+      {
+        icon = '🔍 ',
+        icon_hl = 'Title',
+        desc = 'Find Text',
+       desc_hl = 'String',
+       key = 'g',
+       key_hl = 'Number',
+       key_format = ' %s', -- remove default surrounding `[]`
+       action = 'Telescope grep_string'
+      },
+      {
+        icon = '⚙ ',
+        icon_hl = 'Title',
+        desc = 'Configuration',
+        desc_hl = 'String',
+        key = 'c',
+        key_hl = 'Number',
+        key_format = ' %s', -- remove default surrounding `[]`
+        action = reconfig
+      },
+      {
+        icon = '✕ ',
+        icon_hl = 'Title',
+        desc = 'Close',
+        desc_hl = 'String',
+        key = 'q',
+				key_hl = 'Number',
+        key_format = ' %s', -- remove default surrounding `[]`
+        action = 'q!'
+      },
     },
+
+    week_header = {
+      enable = false,
+    },
+    footer = {
+			'16'
+    },
+
+  },
 }
 
 -- font ansi Shadow link: https://textkool.com/pt/ascii-art-generator
